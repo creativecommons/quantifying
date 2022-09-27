39aa2a47e74c428b330ca043a281bc86a6ba7bf4
Shuran Yang <shuran1030@berkeley.edu>
2022-09-27 02:29:33 -0700

    photos json retrieved

--- flickr/api.py
+++ flickr/api.py
@@ -1,5 +1,6 @@
 import flickrapi
 import json
+import pandas
 
 
 api_key = u'69f154651c83944ef0b07b91c8b87706'
@@ -8,10 +9,13 @@ api_secret = u'df4ab2a4358bc456'
 flickr = flickrapi.FlickrAPI(api_key, api_secret, format='json')
 licenseJson = flickr.photos.licenses.getInfo()
 licenseInfo = json.loads(licenseJson.decode('utf-8'))
-photosJson = flickr.photos.search(license = 1)
-photos = json.loads(photosJson.decode('utf-8'))
-print(photos)
 
-"""for i in [1, 2, 3, 4, 5, 6, 9, 10]:
+dic = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 9: 0, 10: 0}
+for i in dic.keys():
     photosJson = flickr.photos.search(license = i)
-    photos = json.loads(photosJson.decode('utf-8'))"""
+    photos = json.loads(photosJson.decode('utf-8'))
+    dic[i] = photos
+
+with open('photos.json', 'w') as json_file:
+    json.dump(dic, json_file)
+
