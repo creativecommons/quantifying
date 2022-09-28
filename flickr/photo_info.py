320388eca50a98d98ff0cefdafa18b598d68c424
Shuran Yang <shuran1030@berkeley.edu>
2022-09-27 23:14:09 -0700

    fixed pages issue

--- flickr/photo_info.py
+++ flickr/photo_info.py
@@ -9,12 +9,19 @@ api_secret = u'df4ab2a4358bc456'
 flickr = flickrapi.FlickrAPI(api_key, api_secret, format='json')
 
 # be careful to run the below api call (3600 limits/hr)
-dic = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 9: 0, 10: 0}
-keys = [1, 2, 3, 4, 5, 6, 9, 10]
-info_dic = {}
-for j in keys:
-    for k in range(0, len((photo[str(j)]["photos"]["photo"]).keys())):
-        info_dic[photo[str(j)]["photos"]["photo"][k]["id"]] = flickr.photos.getInfo(photo[str(j)]["photos"]["photo"][k]["id"])
+# dic = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 9: 0, 10: 0}
+# keys = [1, 2, 3, 4, 5, 6, 9, 10]
+# info_dic = {}
+# for j in keys:
+#     for k in range(0, len((photo[str(j)]["photos"]["photo"]).keys())):
+#         info_dic[photo[str(j)]["photos"]["photo"][k]["id"]] = flickr.photos.getInfo(photo[str(j)]["photos"]["photo"][k]["id"])
 
-#print(len(photo[str(1)]["photos"]["photo"]))
+print(len(photo[str(1)]["photos"]["photo"]))
+print(len(photo[str(2)]["photos"]["photo"]))
+print(len(photo[str(3)]["photos"]["photo"]))
+print(len(photo[str(4)]["photos"]["photo"]))
+print(len(photo[str(5)]["photos"]["photo"]))
+print(len(photo[str(6)]["photos"]["photo"]))
+print(len(photo[str(9)]["photos"]["photo"]))
+print(len(photo[str(10)]["photos"]["photo"]))
 #print(photo[str(1)]["photos"]["photo"][1]["id"])
