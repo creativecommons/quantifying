e2c83c03cf8479bae3fc463467dd8a70f127fc0a
Brandon Huang <bsbanstudme@gmail.com>
2022-10-20 23:40:06 -0700

    expo backoff, code quality, file extension update

--- internetarchive/internetarchive_scratcher.py
+++ internetarchive/internetarchive_scratcher.py
@@ -6,47 +6,28 @@ Data.
 # Standard library
 import datetime as dt
 import os
-import random
 import sys
-import time
 import traceback
 
 # Third-party
 import pandas as pd
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 # First-party/Local
 from internetarchive.search import Search
 from internetarchive.session import ArchiveSession
 
+today = dt.datetime.today()
 CWD = os.path.dirname(os.path.abspath(__file__))
-CALLBACK_INDEX = 2
-CALLBACK_EXPO = 0
-MAX_WAIT = 64
-DATA_WRITE_FILE = CWD
-
-
-def expo_backoff():
-    """Performs exponential backoff upon call.
-    The function will force a wait of CALLBACK_INDEX ** CALLBACK_EXPO + r
-    seconds, where r is a decimal number between 0.001 and 0.999, inclusive.
-    If that value is higher than MAX_WAIT, then it will just wait MAX_WAIT
-    seconds instead.
-    """
-    global CALLBACK_EXPO
-    backoff = random.randint(1, 1000) / 1000 + CALLBACK_INDEX**CALLBACK_EXPO
-    time.sleep(min(backoff, MAX_WAIT))
-    if backoff < MAX_WAIT:
-        CALLBACK_EXPO += 1
-
-
-def expo_backoff_reset():
-    """Resets the CALLBACK_EXPO to 0."""
-    global CALLBACK_EXPO
-    CALLBACK_EXPO = 0
+DATA_WRITE_FILE = (
+    f"{CWD}"
+    f"/data_internetarchive_{today.year}_{today.month}_{today.day}.csv"
+)
 
 
 def get_license_list():
-    """Provides the list of license from 2018's record of Creative Commons.
+    """Provides the list of license from a Creative Commons provided tool list.
     Returns:
         np.array: An np array containing all license types that should be
         searched via Programmable Search Engine.
@@ -56,7 +37,7 @@ def get_license_list():
     return license_list
 
 
-def get_response_elems(license, eb=False):
+def get_response_elems(license):
     """Provides the metadata for query of specified parameters
     Args:
         license:
@@ -64,30 +45,32 @@ def get_response_elems(license, eb=False):
             of its URL towards the license description. Alternatively, the
             default None value stands for having no assumption about license
             type.
-        eb:
-            A boolean indicating whether there should be exponential callback.
-            Is by default False.
 
     Returns:
         dict: A dictionary mapping metadata to its value provided from the API
         query of specified parameters.
     """
     try:
-        elem_session = ArchiveSession()
+        max_retries = Retry(
+            total=5,
+            backoff_factor=10,
+            status_forcelist=[403, 408, 429, 500, 502, 503, 504],
+        )
+        search_session = ArchiveSession()
+        search_session.mount_http_adapter(
+            protocol="https://",
+            max_retries=HTTPAdapter(max_retries=max_retries),
+        )
         search_data = Search(
-            elem_session,
+            search_session,
             f'/metadata/licenseurl:("http://creativecommons.org/{license}")',
         )
         search_data_dict = {"totalResults": len(search_data)}
         return search_data_dict
-    except:
-        if eb:
-            expo_backoff()
-            get_response_elems(license)
-        else:
-            print("ERROR (1) Unhandled exception:", file=sys.stderr)
-            print(traceback.print_exc(), file=sys.stderr)
-            sys.exit(1)
+    except Exception:
+        print("ERROR (1) Unhandled exception:", file=sys.stderr)
+        print(traceback.print_exc(), file=sys.stderr)
+        sys.exit(1)
 
 
 def set_up_data_file():
@@ -100,7 +83,7 @@ def set_up_data_file():
 def record_license_data(license_type):
     """Writes the row for LICENSE_TYPE to file to contain IA Query data.
     Args:
-        license:
+        license_type:
             A string representing the type of license, and should be a segment
             of its URL towards the license description. Alternatively, the
             default None value stands for having no assumption about license
@@ -116,7 +99,7 @@ def record_license_data(license_type):
 
 def record_all_licenses():
     """Records the data of all license types findable in the license list and
-    records these data into the DATA_wRITE_FILE as specified in that constant.
+    records these data into the DATA_WRITE_FILE as specified in that constant.
     """
     license_list = get_license_list()
     for license_type in license_list:
@@ -125,14 +108,8 @@ def record_all_licenses():
 
 
 def main():
-    global DATA_WRITE_FILE
-    today = dt.datetime.today()
-    DATA_WRITE_FILE += (
-        f"/data_internetarchive_{today.year}_{today.month}_{today.day}.txt"
-    )
     set_up_data_file()
     record_all_licenses()
-    DATA_WRITE_FILE = CWD
 
 
 if __name__ == "__main__":
