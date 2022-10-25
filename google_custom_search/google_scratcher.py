76e93ff6bb3c4e5984737e0e01c38a6bd9b7f1f3
Brandon Huang <bsbanstudme@gmail.com>
2022-10-24 22:21:55 -0700

    expo feedback, general code quality update

--- google_custom_search/google_scratcher.py
+++ google_custom_search/google_scratcher.py
@@ -1,49 +1,29 @@
 """
-This file is dedicated to obtain a .csv record report for Google Custom Search 
+This file is dedicated to obtain a .csv record report for Google Custom Search
 Data.
 """
 
 # Standard library
 import datetime as dt
 import os
-import random
 import sys
-import time
 import traceback
 
 # Third-party
 import pandas as pd
 import query_secret
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
-CWD = os.path.dirname(os.path.abspath(__file__))
-CALLBACK_INDEX = 2
-CALLBACK_EXPO = 0
-MAX_WAIT = 64
-DATA_WRITE_FILE = CWD
+today = dt.datetime.today()
 API_KEY = query_secret.API_KEY
+CWD = os.path.dirname(os.path.abspath(__file__))
+DATA_WRITE_FILE = (
+    f"{CWD}"
+    f"/data_google_custom_search_{today.year}_{today.month}_{today.day}.csv"
+)
 PSE_KEY = query_secret.PSE_KEY
-
-
-def expo_backoff():
-    """Performs exponential backoff upon call.
-
-    The function will force a wait of CALLBACK_INDEX ** CALLBACK_EXPO + r
-    seconds, where r is a decimal number between 0.001 and 0.999, inclusive.
-    If that value is higher than MAX_WAIT, then it will just wait MAX_WAIT
-    seconds instead.
-    """
-    global CALLBACK_EXPO
-    backoff = random.randint(1, 1000) / 1000 + CALLBACK_INDEX**CALLBACK_EXPO
-    time.sleep(min(backoff, MAX_WAIT))
-    if backoff < MAX_WAIT:
-        CALLBACK_EXPO += 1
-
-
-def expo_backoff_reset():
-    """Resets the CALLBACK_EXPO to 0."""
-    global CALLBACK_EXPO
-    CALLBACK_EXPO = 0
 
 
 def get_license_list():
@@ -71,22 +51,27 @@ def get_lang_list():
         pd.DataFrame: A Dataframe whose index is language name and has a column
         for the corresponding language code.
     """
-    langs = pd.read_csv(f"{CWD}/google_lang.txt", sep=":")
-    langs = langs.set_index("Language")
-    selected_langs = langs.iloc[[7, 33, 34, 8, 11, 0, 25, 14], :].sort_index()
-    return selected_langs
+    languages = pd.read_csv(
+        f"{CWD}/google_lang.txt", sep=": ", header=None, engine="python"
+    )
+    languages[0] = languages[0].str.extract(r'"(\w+)"')
+    languages = languages.set_index(1)
+    selected_languages = languages.iloc[
+        [7, 33, 34, 8, 11, 0, 25, 14], :
+    ].sort_index()
+    return selected_languages
 
 
-def get_cntr_list():
+def get_country_list():
     """Provides the list of countries to find Creative Commons usage data on.
 
     Returns:
         pd.DataFrame: A Dataframe whose index is country name and has a column
         for the corresponding country code.
     """
-    cntrs = pd.read_csv(CWD + "/google_cntrs.txt", sep="\t")
-    cntrs = cntrs.set_index("Country")
-    selected_cntrs = cntrs.loc[
+    countries = pd.read_csv(CWD + "/google_countries.txt", sep="\t")
+    countries = countries.set_index("Country")
+    selected_countries = countries.loc[
         [
             "India",
             "Japan",
@@ -101,10 +86,10 @@ def get_cntr_list():
         ],
         :,
     ].sort_index()
-    return selected_cntrs
+    return selected_countries
 
 
-def get_request_url(license=None, cntr=None, lang=None):
+def get_request_url(license=None, country=None, language=None):
     """Provides the API Endpoint URL for specified parameter combinations.
 
     Args:
@@ -113,12 +98,12 @@ def get_request_url(license=None, cntr=None, lang=None):
             of its URL towards the license description. Alternatively, the
             default None value stands for having no assumption about license
             type.
-        cntr:
+        country:
             A string representing the country code of country that the search
             results would be originating from. Alternatively, the default None
             value or "all" stands for having no assumption about country of
             origin.
-        lang:
+        language:
             A string representing the language that the search results are
             presented in. Alternatively, the default None value or "all" stands
             for having no assumption about language of document.
@@ -136,14 +121,14 @@ def get_request_url(license=None, cntr=None, lang=None):
         base_url += license.replace("/", "%2F")
     else:
         base_url += "/licenses".replace("/", "%2F")
-    if cntr is not None:
-        base_url += f"&cr={cntr}"
-    if lang is not None:
-        base_url += f"&lr={lang}"
+    if country is not None:
+        base_url += f"&cr={country}"
+    if language is not None:
+        base_url += f"&lr={language}"
     return base_url
 
 
-def get_response_elems(license=None, cntr=None, lang=None, eb=False):
+def get_response_elems(license=None, country=None, language=None):
     """Provides the metadata for query of specified parameters
 
     Args:
@@ -152,7 +137,7 @@ def get_response_elems(license=None, cntr=None, lang=None, eb=False):
             of its URL towards the license description. Alternatively, the
             default None value stands for having no assumption about license
             type.
-        cntr:
+        country:
             A string representing the country code of country that the search
             results would be originating from. Alternatively, the default None
             value or "all" stands for having no assumption about country of
@@ -161,28 +146,31 @@ def get_response_elems(license=None, cntr=None, lang=None, eb=False):
             A string representing the language that the search results are
             presented in. Alternatively, the default None value or "all" stands
             for having no assumption about language of document.
-        eb:
-            A boolean indicating whether there should be exponential callback.
-            Is by default False.
 
     Returns:
         dict: A dictionary mapping metadata to its value provided from the API
         query of specified parameters.
     """
     try:
-        url = get_request_url(license=license, cntr=cntr, lang=lang)
-        search_data = requests.get(url).json()
+        request_url = get_request_url(license, country, language)
+        max_retries = Retry(
+            total=5,
+            backoff_factor=10,
+            status_forcelist=[403, 408, 429, 500, 502, 503, 504],
+        )
+        session = requests.Session()
+        session.mount("https://", HTTPAdapter(max_retries=max_retries))
+        with session.get(request_url) as response:
+            response.raise_for_status()
+            search_data = response.json()
         search_data_dict = {
             "totalResults": search_data["queries"]["request"][0][
                 "totalResults"
             ]
         }
         return search_data_dict
-    except:
-        if eb:
-            expo_backoff()
-            get_response_elems(license, cntr, lang)
-        elif "queries" not in search_data:
+    except Exception:
+        if "queries" not in search_data:
             print(search_data)
             sys.exit(1)
         else:
@@ -194,12 +182,12 @@ def get_response_elems(license=None, cntr=None, lang=None, eb=False):
 def set_up_data_file():
     """Writes the header row to file to contain Google Query data."""
     header_title = "LICENSE TYPE,No Priori,"
-    selected_cntrs = get_cntr_list()
-    selected_langs = get_lang_list()
+    selected_countries = get_country_list()
+    selected_languages = get_lang_list()
     header_title = (
         "LICENSE TYPE,No Priori,"
-        f"{','.join(selected_cntrs.index)}"
-        f"{','.join(selected_langs.index)}"
+        f"{','.join(selected_countries.index)}"
+        f"{','.join(selected_languages.index)}"
     )
     with open(DATA_WRITE_FILE, "a") as f:
         f.write(header_title + "\n")
@@ -219,15 +207,17 @@ def record_license_data(license_type=None):
         data_log = "all,"
     else:
         data_log = f"{license_type},"
-    selected_cntrs = get_cntr_list()
-    selected_langs = get_lang_list()
+    selected_countries = get_country_list()
+    selected_languages = get_lang_list()
     no_priori_search = get_response_elems(license=license_type)
     data_log += f"{no_priori_search['totalResults']},"
-    for cntr_name in selected_cntrs.iloc[:, 0]:
-        response = get_response_elems(license=license_type, cntr=cntr_name)
+    for country_name in selected_countries.iloc[:, 0]:
+        response = get_response_elems(
+            license=license_type, country=country_name
+        )
         data_log += f"{response['totalResults']},"
-    for lang_name in selected_langs.iloc[:, 0]:
-        response = get_response_elems(license=license_type, lang=lang_name)
+    for lang_name in selected_languages.iloc[:, 0]:
+        response = get_response_elems(license=license_type, language=lang_name)
         data_log += f"{response['totalResults']},"
     with open(DATA_WRITE_FILE, "a") as f:
         f.write(f"{data_log}\n")
@@ -254,15 +244,8 @@ def get_current_data():
 
 
 def main():
-    # TODO
-    global DATA_WRITE_FILE
-    today = dt.datetime.today()
-    DATA_WRITE_FILE += (
-        f"/data_google_{today.year}_{today.month}_{today.day}.txt"
-    )
     set_up_data_file()
     record_all_licenses()
-    DATA_WRITE_FILE = CWD
 
 
 if __name__ == "__main__":
