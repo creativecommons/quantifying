25106eb18f548d5e5bd0fa8d50c5f3b58ec28cb6
Timid Robot Zehta <timid@creativecommons.org>
2022-11-01 13:55:56 -0700

    add additional context for secrets

--- vimeo/query_secrets.example.py
+++ vimeo/query_secrets.example.py
@@ -1,2 +1,7 @@
+# "Before we set you loose on the API, we ask that you provide a little
+# information about your app. An app in this sense can be a full-featured
+# mobile application, a dynamic web page, or a three-line script. If it's
+# making API calls, it's an app."
+# https://developer.vimeo.com/api/guides/start#register-your-app
 ACCESS_TOKEN = ""
 CLIENT_ID = ""
