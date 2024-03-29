8eea286252c9fc8a9fe2fcc134f397128a21b939
Darylgolden <darylgolden@gmail.com>
2024-03-29 19:32:57 +0800

    Add github scratcher

---
+++ github/github_scratcher.py
@@ -0,0 +1,111 @@
+import json
+import os
+import os.path
+import sys
+import traceback
+from urllib import request
+
+sys.path.append(".")
+
+import requests
+import quantify
+import pandas as pd
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+PATH_REPO_ROOT, PATH_WORK_DIR, PATH_DOTENV, DATETIME_TODAY = quantify.setup(
+    __file__
+)
+
+DATA_WRITE_FILE = os.path.join(
+    PATH_WORK_DIR,
+    f"data_github_"
+    f"{DATETIME_TODAY.year}_{DATETIME_TODAY.month}_{DATETIME_TODAY.day}.csv",
+)
+
+def set_up_data_file():
+    """Writes the header row of the data file."""
+    header_title = "LICENSE_TYPE,Repository Count"
+    with open(DATA_WRITE_FILE, "w") as f:
+        f.write(header_title + "\n")
+
+# def get_license_list():
+#     """Provides the list of license from a Creative Commons provided tool list.
+#     Returns:
+#         np.array: An np array containing all license types that should be
+#         searched via Programmable Search Engine.
+#     """
+#     cc_license_data = pd.read_csv(
+#         os.path.join(PATH_WORK_DIR, "legal-tool-paths.txt"), header=None
+#     )
+#     license_list = cc_license_data[0].unique()
+#     return license_list
+
+def get_response_elems(license):
+    """Provides the metadata for query of specified parameters
+    Args:
+        license:
+            A string representing the type of license, and should be a segment
+            of its URL towards the license description. Alternatively, the
+            default None value stands for having no assumption about license
+            type.
+    Returns:
+        dict: A dictionary mapping metadata to its value provided from the API
+        query of specified parameters.
+    """
+    try:
+        base_url = "https://api.github.com/search/repositories?q=license:"
+        request_url = f"{base_url}{license}"
+        max_retries = Retry(
+            total=5,
+            backoff_factor=10,
+            status_forcelist=[403, 408, 429, 500, 502, 503, 504],
+        )
+        session = requests.Session()
+        session.mount("https://", HTTPAdapter(max_retries=max_retries))
+        with session.get(request_url) as response:
+            response.raise_for_status()
+            search_data = response.json()
+        return {"totalResults": search_data["total_count"]}
+    except Exception as e:
+        raise e
+    
+def record_license_data(license_type):
+    """Writes the row for LICENSE_TYPE to file to contain Github Query data.
+    Args:
+        license_type:
+            A string representing the type of license, and should be a segment
+            of its URL towards the license description. Alternatively, the
+            default None value stands for having no assumption about license
+            type.
+    """
+    data_log = (
+        f"{license_type},"
+        f"{get_response_elems(license_type)['totalResults']}"
+    )
+    with open(DATA_WRITE_FILE, "a") as f:
+        f.write(f"{data_log}\n")
+
+def record_all_licenses():
+    """Records the data of all license types findable in the license list and
+    records these data into the DATA_WRITE_FILE as specified in that constant.
+    """
+    record_license_data("CC")
+
+def main():
+    set_up_data_file()
+    record_all_licenses()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except SystemExit as e:
+        sys.exit(e.code)
+    except KeyboardInterrupt:
+        print("INFO (130) Halted via KeyboardInterrupt.", file=sys.stderr)
+        sys.exit(130)
+    except Exception:
+        print("ERROR (1) Unhandled exception:", file=sys.stderr)
+        print(traceback.print_exc(), file=sys.stderr)
+        sys.exit(1)
