bf9357539e9eb75315f10b8acee96d53ab899074
Timid Robot Zehta <timid@creativecommons.org>
2024-10-02 10:48:51 -0700

    add commit message to log

--- scripts/shared.py
+++ scripts/shared.py
@@ -95,7 +95,7 @@ def add_and_commit(repo_path, add_path, message):
             return
         repo.index.add([add_path])
         repo.index.commit(message)
-        logging.info("Changes committed")
+        logging.info(f"Changes committed: {message}")
     except InvalidGitRepositoryError:
         raise QuantifyingException(f"Invalid Git repository at {repo_path}", 2)
     except NoSuchPathError:
