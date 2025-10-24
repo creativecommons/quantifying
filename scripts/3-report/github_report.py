223c04366a8aaaf810f346556ddaa8bb803aa76e
Oreoluwa Oluwasina <oreoluwaoluwasina@gmail.com>
2025-10-24 01:09:42 +0100

    Github processing and reporting

--- scripts/3-report/github_report.py
+++ scripts/3-report/github_report.py
@@ -7,23 +7,26 @@ from GitHub.
 import argparse
 import os
 import sys
+import textwrap
 import traceback
-from datetime import datetime, timezone
 
 # Third-party
-import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
-from pandas import PeriodIndex
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import PythonTracebackLexer
 
 # Add parent directory so shared can be imported
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 # First-party/Local
+import plot  # noqa: E402
 import shared  # noqa: E402
 
 # Setup
 LOGGER, PATHS = shared.setup(__file__)
+QUARTER = os.path.basename(PATHS["data_quarter"])
+SECTION = "Github Data"
 
 
 def parse_arguments():
@@ -31,36 +34,36 @@ def parse_arguments():
     Parses command-line arguments, returns parsed arguments.
     """
     LOGGER.info("Parsing command-line arguments")
-    # Taken from shared module, fix later
-    datetime_today = datetime.now(timezone.utc)
-    quarter = PeriodIndex([datetime_today.date()], freq="Q")[0]
-
-    parser = argparse.ArgumentParser(description="GitHub Data Report")
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
         "--quarter",
-        "-q",
-        type=str,
-        default=f"{quarter}",
-        help="Data quarter in format YYYYQx, e.g., 2024Q2",
-    )
-    parser.add_argument(
-        "--skip-commit",
-        action="store_true",
-        help="Don't git commit changes (also skips git push changes)",
-    )
-    parser.add_argument(
-        "--skip-push",
-        action="store_true",
-        help="Don't git push changes",
+        default=QUARTER,
+        help=f"Data quarter in format YYYYQx (default: {QUARTER})",
     )
     parser.add_argument(
         "--show-plots",
         action="store_true",
-        help="Show generated plots (in addition to saving them)",
+        help="Show generated plots (default: False)",
+    )
+    parser.add_argument(
+        "--enable-save",
+        action="store_true",
+        help="Enable saving results (default: False)",
+    )
+    parser.add_argument(
+        "--enable-git",
+        action="store_true",
+        help="Enable git actions such as fetch, merge, add, commit, and push"
+        " (default: False)",
     )
     args = parser.parse_args()
-    if args.skip_commit:
-        args.skip_push = True
+    if not args.enable_save and args.enable_git:
+        parser.error("--enable-git requires --enable-save")
+    if args.quarter != QUARTER:
+        global PATHS
+        PATHS = shared.paths_update(LOGGER, PATHS, QUARTER, args.quarter)
+    args.logger = LOGGER
+    args.paths = PATHS
     return args
 
 
@@ -71,7 +74,7 @@ def load_data(args):
     selected_quarter = args.quarter
 
     file_path = os.path.join(
-        PATHS["data"], f"{selected_quarter}", "1-fetch", "github_fetched.csv"
+        PATHS["data"], f"{selected_quarter}", "1-fetch", "github_1_count.csv"
     )
 
     if not os.path.exists(file_path):
@@ -83,99 +86,147 @@ def load_data(args):
     return data
 
 
-def visualize_by_license_type(data, args):
+def Plot_by_license_type(args):
     """
-    Create a bar chart for the number of repositories licensed by license type.
+    Create plots for the languages with highest usage of latest tools
     """
-    LOGGER.info(
-        "Creating a bar chart for the number "
-        "of repositories licensed by license type."
+    LOGGER.info(plot_totals_by_code_license.__doc__.strip())
+    file_path = shared.path_join(
+        PATHS["data_1-fetch"],
+        "github_1_count.csv",
+    )
+    LOGGER.info(f"data file: {file_path.replace(PATHS['repo'], '.')}")
+    name_label = "TOOL_IDENTIFIER"
+    data_label = "COUNT"
+    data = pd.read_csv(file_path, index_col=name_label)
+
+    title = "Totals by license type"
+    plt = plot.combined_plot(
+        args=args,
+        data=data,
+        title=title,
+        name_label=name_label,
+        data_label=data_label,
     )
 
-    selected_quarter = args.quarter
-
-    # Strip any leading/trailing spaces from the columns
-    data.columns = data.columns.str.strip()
-
-    plt.figure(figsize=(12, 8))
-    try:
-        ax = sns.barplot(x=data["LICENSE_TYPE"], y=data["Repository Count"])
-    except KeyError as e:
-        LOGGER.error(f"KeyError: {e}. Available columns are: {data.columns}")
-        return
-
-    plt.title("Number of Repositories Licensed by License Type")
-    plt.xlabel("License Type")
-    plt.ylabel("Number of Repositories")
-    plt.xticks(rotation=45, ha="right")
-
-    # Add value numbers to the top of each bar
-    for p in ax.patches:
-        ax.annotate(
-            format(p.get_height(), ",.0f"),
-            (p.get_x() + p.get_width() / 2.0, p.get_height()),
-            ha="center",
-            va="center",
-            xytext=(0, 9),
-            textcoords="offset points",
-        )
-
-    output_directory = os.path.join(
-        PATHS["data"], f"{selected_quarter}", "3-report"
+    image_path = shared.path_join(
+        PATHS["data_phase"], "github_totals_by_license_type.png"
     )
+    LOGGER.info(f"image file: {image_path.replace(PATHS['repo'], '.')}")
 
-    LOGGER.info(f"Output directory: {output_directory}")
-
-    # Create the directory if it does not exist
-    os.makedirs(output_directory, exist_ok=True)
-    image_path = os.path.join(
-        output_directory, "github_license_type_report.png"
-    )
-
-    plt.savefig(image_path)
-
-    if args.show_plots:
-        plt.show()
+    if args.enable_save:
+        # Create the directory if it does not exist
+        os.makedirs(PATHS["data_phase"], exist_ok=True)
+        plt.savefig(image_path)
 
     shared.update_readme(
-        PATHS,
-        image_path,
-        "GitHub Data",
-        "Number of Repositories Licensed by License Type",
-        "GitHub License Type Report",
         args,
+        SECTION,
+        title,
+        image_path,
+        "Plots showing totals by license type.",
     )
 
-    LOGGER.info("Visualization by license type created.")
+
+def plot_totals_by_code_license(args):
+    """
+    Create plots for the languages with highest usage of latest tools
+    """
+    LOGGER.info(plot_totals_by_code_license.__doc__.strip())
+    file_path = shared.path_join(
+        PATHS["data_2-process"],
+        "github_totals_by_code_license.csv",
+    )
+    LOGGER.info(f"data file: {file_path.replace(PATHS['repo'], '.')}")
+    name_label = "Category"
+    data_label = "Count"
+    data = pd.read_csv(file_path, index_col=name_label)
+
+    title = "Totals by Code License"
+    plt = plot.combined_plot(
+        args=args,
+        data=data,
+        title=title,
+        name_label=name_label,
+        data_label=data_label,
+    )
+
+    image_path = shared.path_join(
+        PATHS["data_phase"], "github_totals_code_license.png"
+    )
+    LOGGER.info(f"image file: {image_path.replace(PATHS['repo'], '.')}")
+
+    if args.enable_save:
+        # Create the directory if it does not exist
+        os.makedirs(PATHS["data_phase"], exist_ok=True)
+        plt.savefig(image_path)
+
+    shared.update_readme(
+        args,
+        SECTION,
+        title,
+        image_path,
+        "Plots showing totals by code license.",
+    )
+
+
+def plot_totals_by_restriction(args):
+    """
+    Create plots for the languages with highest usage of latest tools
+    """
+    LOGGER.info(plot_totals_by_restriction.__doc__.strip())
+    file_path = shared.path_join(
+        PATHS["data_2-process"],
+        "github_totals_by_restriction.csv",
+    )
+    LOGGER.info(f"data file: {file_path.replace(PATHS['repo'], '.')}")
+    name_label = "Category"
+    data_label = "Count"
+    data = pd.read_csv(file_path, index_col=name_label)
+
+    title = "Approved for Free Cultural Works"
+    plt = plot.combined_plot(
+        args=args,
+        data=data,
+        title=title,
+        name_label=name_label,
+        data_label=data_label,
+    )
+
+    image_path = shared.path_join(
+        PATHS["data_phase"], "github_restriction.png"
+    )
+    LOGGER.info(f"image file: {image_path.replace(PATHS['repo'], '.')}")
+    if args.enable_save:
+        # Create the directory if it does not exist
+        os.makedirs(PATHS["data_phase"], exist_ok=True)
+        plt.savefig(image_path)
+
+    shared.update_readme(
+        args,
+        SECTION,
+        title,
+        image_path,
+        "Plots showing totals by restrictions.",
+    )
 
 
 def main():
-
-    # Fetch and merge changes
-    shared.fetch_and_merge(PATHS["repo"])
-
     args = parse_arguments()
-
-    data = load_data(args)
-    if data.empty:
-        return
-
-    current_directory = os.getcwd()
-    LOGGER.info(f"Current working directory: {current_directory}")
-
-    visualize_by_license_type(data, args)
+    shared.paths_log(LOGGER, PATHS)
+    shared.git_fetch_and_merge(args, PATHS["repo"])
+    plot_totals_by_restriction(args)
+    plot_totals_by_code_license(args)
+    Plot_by_license_type(args)
 
     # Add and commit changes
-    if not args.skip_commit:
-        shared.add_and_commit(
-            PATHS["repo"],
-            PATHS["data_quarter"],
-            "Add and commit new GitHub reports",
-        )
-
-    # Push changes
-    if not args.skip_push:
-        shared.push_changes(PATHS["repo"])
+    args = shared.git_add_and_commit(
+        args,
+        PATHS["repo"],
+        PATHS["data_quarter"],
+        f"Add and commit Github reports for {QUARTER}",
+    )
+    shared.git_push_changes(args, PATHS["repo"])
 
 
 if __name__ == "__main__":
@@ -188,11 +239,20 @@ if __name__ == "__main__":
             LOGGER.error(e.message)
         sys.exit(e.exit_code)
     except SystemExit as e:
-        LOGGER.error(f"System exit with code: {e.code}")
+        if e.code != 0:
+            LOGGER.error(f"System exit with code: {e.code}")
         sys.exit(e.code)
     except KeyboardInterrupt:
         LOGGER.info("(130) Halted via KeyboardInterrupt.")
         sys.exit(130)
     except Exception:
-        LOGGER.exception(f"(1) Unhandled exception: {traceback.format_exc()}")
+        traceback_formatted = textwrap.indent(
+            highlight(
+                traceback.format_exc(),
+                PythonTracebackLexer(),
+                TerminalFormatter(),
+            ),
+            "    ",
+        )
+        LOGGER.critical(f"(1) Unhandled exception:\n{traceback_formatted}")
         sys.exit(1)
